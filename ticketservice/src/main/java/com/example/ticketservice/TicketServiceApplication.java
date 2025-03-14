dcf790dc3e792bbf256154f36f6dd247943a198a
ouard1 <boumansour.ouarda@gmail.com>
2025-03-15 00:41:13 +0100

    fix service ticket

--- ticketservice/src/main/java/com/example/ticketservice/TicketServiceApplication.java
+++ ticketservice/src/main/java/com/example/ticketservice/TicketServiceApplication.java
@@ -3,7 +3,7 @@ package com.example.ticketservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.ticketservice")
 public class TicketServiceApplication {
 
 	public static void main(String[] args) {
