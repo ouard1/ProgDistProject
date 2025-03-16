05580fc95fb72d09c48846c37f0caaa90b128f61
ouard1 <boumansour.ouarda@gmail.com>
2025-03-16 18:09:20 +0100

    updates

---
+++ ticketservice/src/main/java/com/example/ticketservice/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.ticketservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String testEndpoint() {
+        return "Test endpoint works!";
+    }
+}
